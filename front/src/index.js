550b85a34e0cfaca39366032e929a9a94152aeca
jeanvaes <jeanvaes@unisabana.edu.co>
2024-03-06 18:55:50 -0500

    Cambiar diseño del frontend

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 
 const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer);
@@ -23,16 +24,38 @@ const GetPokemonAbility = () => {
 
     return (
         <div>
-            <input type="text" value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
-            <button onClick={handleGetPokemonAbility}>Get Pokemon Ability</button>
+            <div className="input-container">
+                <label htmlFor="pokemon-name">Pokemon Name:</label>
+                <input id="pokemon-name" type="text" value={pokemonName}
+                       onChange={e => setPokemonName(e.target.value)}/>
+                <button onClick={handleGetPokemonAbility}>Get Pokemon Ability</button>
+            </div>
             {error && <p>{error}</p>}
             {pokemonData && (
-                <div>
-                    <h2>{pokemonData.name}</h2>
-                    <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+                <div className="pokemon-data">
+                    <h2>{pokemonData.name} (ID: {pokemonData.id})</h2>
+                    <img src={pokemonData.sprites.front_default} alt={pokemonData.name}/>
                     <p>Experience: {pokemonData.base_experience}</p>
                     <p>Height: {pokemonData.height}</p>
                     <p>Weight: {pokemonData.weight}</p>
+                    <h3>Abilities:</h3>
+                    <ul>
+                        {pokemonData.abilities.map((ability, index) => (
+                            <li key={index}>{ability.ability.name}</li>
+                        ))}
+                    </ul>
+                    <h3>Stats:</h3>
+                    <ul>
+                        {pokemonData.stats.map((stat, index) => (
+                            <li key={index}>{stat.stat.name}: {stat.base_stat}</li>
+                        ))}
+                    </ul>
+                    <h3>Moves:</h3>
+                    <ul>
+                        {pokemonData.moves.map((move, index) => (
+                            <li key={index}>{move.move.name}</li>
+                        ))}
+                    </ul>
                 </div>
             )}
         </div>
